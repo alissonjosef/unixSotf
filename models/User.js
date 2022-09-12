f9639ad34789f11c73163db1e10a1a3deb9b0f69
João Rafael Alvarez <joao@MacBook-Air-de-Joao.local>
2022-09-12 15:03:31 -0300

    Ajustes da validaçao do model User

--- models/User.js
+++ models/User.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose')
+const validators = require('mongoose-validators');
 
 const User = mongoose.model('User', {
-    registry: String,
-    password: String,
+    nome: { type: String, trim: true, required: [true, '{PATH} do usuario é um campo obrigatório'] },
+    cpf: { type: String, trim: true, unique: false, required: [true, '{PATH} do usuario é um campo obrigatório'] },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, '{PATH} do usuario é um campo obrigatório'],
+        validate: validators.isEmail({ message: '{VALUE} não é um {PATH} válido' }),
+    },
+    password: { type: String, required: [true, '{PATH} do usuario é um campo obrigatório'] },
+    perfil: { type: String, required: [true, '{PATH} do usuario é um campo obrigatório'] },
+    registry: { type: String, unique: true, required: [true, '{PATH} do usuario é um campo obrigatório'] },
 })
 
 module.exports = User
