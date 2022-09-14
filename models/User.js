dfff5cc04f38abe57dc0ae284fde2e208b1fce03
unknown <alissonjosef@gmail.com>
2022-09-14 16:21:52 -0300

    add name email profile

--- models/User.js
+++ models/User.js
@@ -1,18 +1,41 @@
-const mongoose = require('mongoose')
-const validators = require('mongoose-validators');
+const mongoose = require("mongoose");
+const validators = require("mongoose-validators");
 
-const User = mongoose.model('User', {
-    nome: { type: String, trim: true, required: [true, '{PATH} do usuario é um campo obrigatório'] },
-    cpf: { type: String, trim: true, unique: false, required: [true, '{PATH} do usuario é um campo obrigatório'] },
-    email: {
-        type: String,
-        unique: true,
-        required: [true, '{PATH} do usuario é um campo obrigatório'],
-        validate: validators.isEmail({ message: '{VALUE} não é um {PATH} válido' }),
-    },
-    password: { type: String, required: [true, '{PATH} do usuario é um campo obrigatório'] },
-    perfil: { type: String, required: [true, '{PATH} do usuario é um campo obrigatório'] },
-    registry: { type: String, unique: true, required: [true, '{PATH} do usuario é um campo obrigatório'] },
-})
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    trim: true,
+    required: [true, "{PATH} do usuario é um campo obrigatório"],
+  },
+  cpf: {
+    type: String,
+    trim: true,
+    unique: false,
+    required: [true, "{PATH} do usuario é um campo obrigatório"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "{PATH} do usuario é um campo obrigatório"],
+    validate: validators.isEmail({ message: "{VALUE} não é um {PATH} válido" }),
+  },
+  password: {
+    type: String,
+    required: [true, "{PATH} do usuario é um campo obrigatório"],
+  },
+  profile: {
+    type: String,
+    required: [true, "{PATH} do usuario é um campo obrigatório"],
+  },
+  registry: {
+    type: String,
+    unique: true,
+    required: [true, "{PATH} do usuario é um campo obrigatório"],
+  },
+  createAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
-module.exports = User
+module.exports = User;
